5e18d0f312631c4b6332d4842ffa6a78a1d1aca6
denisRun <polozov.denis77@gmail.com>
2021-10-12 10:15:42 +0300

    programChanges

--- Program.cs
+++ Program.cs
@@ -10,24 +10,25 @@ namespace Students
     {
         static void Main(string[] args)
         {
-            Student st1 = new Student("a", "a", "a");
+
+            Student studentSvat = new Student("a", "a", "a");
             Student st2 = new Student("b", "b", "b");
             Student st3 = new Student("c", "c", "c");
-            Student st4 = new Student("d", "d", "d");
+            Student studentRab = new Student("d", "d", "d");
 
             Journal j = new Journal();
-            j.AddStudent(st1);
+            j.AddStudent(studentSvat);
             j.AddStudent(st2);
             j.AddStudent(st3);
-            j.AddStudent(st4);
+            j.AddStudent(studentRab);
 
-            j.AddMark(st1, 70);
-            j.AddMark(st1, 90);
+            j.AddMark(studentSvat, 70);
+            j.AddMark(studentSvat, 90);
             j.AddMark(st2, 40);
             j.AddMark(st3, 30);
-            j.AddMark(st4, 65);
-            j.AddMark(st4, 70);
-            Console.WriteLine("Avarage mark " + j.AvgMark(st1));
+            j.AddMark(studentRab, 65);
+            j.AddMark(studentRab, 70);
+            Console.WriteLine("Avarage mark " + j.AvgMark(studentSvat));
 
             Console.WriteLine("Bad Students");
             j.BadStudents();
